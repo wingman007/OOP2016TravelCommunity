6b1f47233aa1ab7298dd832b3d12817686891a81
SvetoslavSlavov <svetoslav_slavov@icloud.com>
2016-06-06 14:04:52 +0300

    Add files via upload

---
+++ TravelCommunity/TravelCommunity/Models/Contact.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TravelCommunity.Models
+{
+    public class Contact
+    {
+        public int id { get; set; }
+        public string Name {get; set; }
+        public string FamilyName { get; set; }
+        public string Adress { get; set; }
+        public string Email { get; set; }
+        public virtual ApplicationUser AppUser {get; set; }
+        public virtual ICollection<Phone> Phones { get; set; }
+    }
+}
---
+++ TravelCommunity/TravelCommunity/Models/Phone.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TravelCommunity.Models
+{
+    public class Phone
+    {
+    
+        public int id { get; set; }
+        public int Number { get; set; }
+        public int contactId { get; set; }
+        public virtual Contact Contact { get; set; }
+    }
+}
